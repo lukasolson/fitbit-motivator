0c1a16fc41c4a18694f8de698ac36907c3d0eb64
Lukas Olson <olson.lukas@gmail.com>
2013-11-08 09:43:31 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,91 @@
+var fs = require("fs"),
+	express = require("express"),
+	app = express(),
+	server = require("http").createServer(app),
+	io = require("socket.io").listen(server);
+
+app.use(express.bodyParser());
+
+var auth = new (require("oauth").OAuth)(
+	"https://api.fitbit.com/oauth/request_token",
+	"https://api.fitbit.com/oauth/access_token",
+	"4aa1a1175b514e6fb15c3f92d5cfaab3",
+	"4ac101158b93458bae083b1b899c2293",
+	"1.0",
+	null,
+	"HMAC-SHA1"
+);
+
+io.set("log level", 1);
+server.listen(1114);
+
+var socketsPerRequestToken = {},
+	socketsPerUserId = {};
+
+io.sockets.on("connection", function (socket) {
+	auth.getOAuthRequestToken(function (error, token, secret) {
+		socketsPerRequestToken[token] = socket;
+		socket.requestToken = token;
+		socket.requestTokenSecret = secret;
+
+		socket.emit("requestToken", token);
+	});
+
+	socket.on("update", function (date) {
+		auth.getProtectedResource("https://api.fitbit.com/1/user/-/activities/date/" + date + ".json", "GET", socket.accessToken, socket.accessTokenSecret, function (error, data) {
+			if (error) return console.log(error);
+
+			data = JSON.parse(data);
+			socket.emit("update", data.summary.steps, data.goals.steps);
+		});
+	});
+
+	socket.on("disconnect", function () {
+		delete socketsPerRequestToken[socket.requestToken];
+		delete socketsPerUserId[socket.userId];
+	});
+});
+
+app.get("/access-token", function (req, res) {
+	res.send(undefined);
+
+	var requestToken = req.query.oauth_token,
+		verifier = req.query.oauth_verifier,
+		socket = socketsPerRequestToken[requestToken],
+		requestTokenSecret = socket.requestTokenSecret;
+
+	auth.getOAuthAccessToken(requestToken, requestTokenSecret, verifier, function (error, token, secret, results) {
+		socket.userId = results.encoded_user_id;
+		socket.accessToken = token;
+		socket.accessTokenSecret = secret;
+		socketsPerUserId[socket.userId] = socket;
+
+		socket.emit("initialized");
+
+		auth.getProtectedResource("https://api.fitbit.com/1/user/-/activities/apiSubscriptions/" + socket.userId + ".json", "POST", token, secret, function (error, data) {});
+	});
+});
+
+// TODO: Verify that this is actually coming from Fitbit
+app.post("/update", function (req, res) {
+	res.send(undefined);
+
+	fs.readFile(req.files.updates.path, function (error, data) {
+		if (error) return console.log(error);
+
+		data = JSON.parse(data);
+		for (var i = 0; i < data.length; i++) {
+			var socket = socketsPerUserId[data[i].ownerId];
+			auth.getProtectedResource("https://api.fitbit.com/1/user/-/activities/date/" + data[i].date + ".json", "GET", socket.accessToken, socket.accessTokenSecret, (function (socket) {
+				return function (error, data) {
+					if (error) return console.log(error);
+
+					data = JSON.parse(data);
+					socket.emit("update", data.summary.steps, data.goals.steps);
+				};
+			})(socket));
+		}
+	});
+});
+
+// *KG^7cFjmBul
---
+++ extension/background.js
@@ -0,0 +1,90 @@
+(function () {
+	var socket = io.connect("http://askullsoon.com:1114"),
+		today = new Date().getDate(),
+		tab,
+		stepsCount,
+		stepsGoal,
+		indicatorIndex;
+
+	socket.on("requestToken", function (requestToken) {
+		chrome.tabs.create({
+			url: "https://www.fitbit.com/oauth/authenticate?oauth_token=" + requestToken
+		}, function (newTab) {
+			tab = newTab;
+		});
+	});
+
+	socket.on("initialized", function () {
+		chrome.tabs.remove(tab.id);
+		socket.emit("update", formatDate(new Date()));
+	});
+
+	socket.on("update", function (newStepsCount, newStepsGoal) {
+		stepsCount = newStepsCount; stepsGoal = newStepsGoal;
+		renderBrowserAction(Math.ceil(calculateMinutesNeeded(stepsCount, stepsGoal, 100)));
+	});
+
+	chrome.alarms.create("motivate", {
+		periodInMinutes: 1
+	});
+
+	chrome.alarms.onAlarm.addListener(function () {
+		if (today !== new Date().getDate()) stepsCount = 0;
+		renderBrowserAction(Math.ceil(calculateMinutesNeeded(stepsCount, stepsGoal, 100)));
+	});
+
+	function formatDate(date) {
+		return date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).substr(-2, 2) + "-" + ("0" + date.getDate()).substr(-2, 2);
+	}
+
+	function calculateMinutesNeeded(stepsCount, stepsGoal, stepsPerMinute) {
+		var now = new Date(),
+			elapsedDayPercent = (now.getHours() + (now.getMinutes() / 60)) / 24,
+			stepsExpected = elapsedDayPercent * stepsGoal,
+			stepsBehind = stepsExpected - stepsCount,
+			stepsNeeded = stepsBehind / (1 - stepsGoal / stepsPerMinute / 60 / 24);
+
+		return stepsBehind > 0 ? stepsNeeded / stepsPerMinute : stepsBehind / (stepsGoal / 24 / 60);
+	}
+
+	function renderBrowserAction(minutesNeeded) {
+		var canvas = document.getElementById("canvas"),
+			context = canvas.getContext("2d");
+
+		var newIndicatorIndex = minutesNeeded <= 0 ? 0 :
+			(minutesNeeded >= 45 ? 45 : Math.ceil(minutesNeeded / 15) * 15);
+
+		if (newIndicatorIndex > indicatorIndex && indicatorIndex > 0) webkitNotifications.createNotification("icon48.png", "Get Walking!", "You need to walk for " + minutesNeeded  + " minute(s).").show();
+		indicatorIndex = newIndicatorIndex;
+
+		var colors = COLORS[indicatorIndex];
+		context.fillStyle = colors[0];
+		context.strokeStyle = colors[1];
+		context.lineWidth = 2;
+
+		context.beginPath();
+		context.arc(9.5, 9.5, 8, 0, Math.PI * 2);
+		context.fill(); context.stroke();
+
+		chrome.browserAction.setIcon({
+			imageData: context.getImageData(0, 0, 19, 19)
+		});
+
+		chrome.browserAction.setBadgeText({
+			text: Math.abs(minutesNeeded) + ""
+		});
+
+		chrome.browserAction.setBadgeBackgroundColor({
+			color: "#677984"
+		});
+
+		chrome.browserAction.setTitle({title: (minutesNeeded > 0 ? "Walk for " : "Rest for ") + Math.abs(minutesNeeded) + " minute(s)"});
+	}
+
+	var COLORS = {
+		0: ["#9ADE11", "#66C009"],
+		15: ["#FFC807", "#F69400"],
+		30: ["#FF6D43", "#D54C01"],
+		45: ["#55C2C2", "#55C2C2"]
+	};
+})();
